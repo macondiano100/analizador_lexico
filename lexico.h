d6c8a956af7dec3608e14e8931c6680d69f4f5a5
Antonio Vidal <antoniovidal100@hotmail.com>
2016-08-27 22:10:01 -0500

    Clase lexico.      Removida la logica de lectura del archivo de la clase lexico.

--- lexico.h
+++ lexico.h
@@ -11,21 +11,13 @@
 #include <string>
 #include <algorithm>
 class Lexico {
-    std::istream &archivo;
-    std::istringstream buffer_linea;
     std::string simbolo;
     int estado;
     bool continua;
     bool error;
-
-    std::string sig_linea();;
-    bool linea_vacia();
-    bool fin_archivo();
     bool is_error() const ;
-    char sig_caracter() ;
     public:
-    explicit Lexico(std::istream &archivo):archivo(archivo),buffer_linea(),simbolo(),
-                                                estado(0),continua(false),error(false) {}
+    explicit Lexico(std::istream &archivo):simbolo(),estado(0),continua(false),error(false) {}
 
 };
 
